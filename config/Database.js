94ed04f0ba6bd29a8f3ab04f8ea4a45a560fc51a
faisal <faisal18ibrahim90@gmail.com>
2023-03-14 12:14:50 +0700

    firt commit

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("bbzuaaeo1qw4zedptwbn", "root", "", {
+const db = new Sequelize("db_mrt", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
